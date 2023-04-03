b75f895160337ab8df444e9b33dc19c8886d18c0
hyunsik kim (kayden) <kayden190116@gamil.com>
2023-04-03 13:41:39 +0930

    edit a little bit for connecting with heroku

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const http = require("http");
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
@@ -9,6 +10,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
+const server = http.createServer(app);
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
@@ -44,5 +46,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('3001 Now listening'));
+    server.listen(PORT, () => console.log('3001 Now listening'));
 });
